9017c8f30d95f8be4966f2a5617297ed05e96c0c
Lushasworld <lucianduarte@yahoo.com>
2026-04-20 01:25:34 -0700

    fix

--- diyps2026/diyps2026.js
+++ diyps2026/diyps2026.js
@@ -1,107 +1,64 @@
-var img;
-var img1;
-var img2;
-var img3;
-var img4;
-var img5;
-var img6;
-var img7;
-var img8;
-var img9;
-var img0;
-
-var initials = 'ld';
-var choice = '1';
-var screenbg;
-var lastscreenshot = 61;
-
-function preload() {
-  // Working images ONLY (no broken links)
-  screenbg = loadImage('https://lushasworld.github.io/images/backgroundextra.png');
-  img = loadImage('https://lushasworld.github.io/images/redline1.png');
-  img1 = loadImage('https://lushasworld.github.io/images/redline1.png');
-  img2 = loadImage('https://lushasworld.github.io/images/purpeline2.png');
-
-  // TEMP copies so nothing breaks
-  img3 = loadImage('https://lushasworld.github.io/images/redline1.png');
-  img4 = loadImage('https://lushasworld.github.io/images/redline1.png');
-  img5 = loadImage('https://lushasworld.github.io/images/redline1.png');
-  img6 = loadImage('https://lushasworld.github.io/images/redline1.png');
-  img7 = loadImage('https://lushasworld.github.io/images/redline1.png');
-  img8 = loadImage('https://lushasworld.github.io/images/redline1.png');
-  img9 = loadImage('https://lushasworld.github.io/images/redline1.png');
-  img0 = loadImage('https://lushasworld.github.io/images/redline1.png');
-}
+let initials = 'ld';
+let choice = '1';
+let lastscreenshot = 61;
+let currentColor;
 
 function setup() {
   createCanvas(600, 600);
-  image(screenbg, 0, 0, width, height);
+  background(245);
+  currentColor = color(255, 0, 0);
 }
 
 function draw() {
-  if (keyIsPressed) {
-    choice = key;
-    clear_print();
-  }
+  fill(240);
+  noStroke();
+  rect(0, 0, 160, 35);
+
+  fill(0);
+  textSize(16);
+  text("Tool: " + choice, 10, 22);
 
   if (mouseIsPressed) {
-    newkeyChoice(choice);
+    noStroke();
+    fill(currentColor);
+    ellipse(mouseX, mouseY, 40, 40);
   }
 }
 
-function newkeyChoice(toolChoice) {
-
-  if (toolChoice == '1') {
-    image(img1, mouseX - 100, mouseY - 50, 200, 100);
-
-  } else if (toolChoice == '2') {
-    image(img2, mouseX - 100, mouseY - 50, 200, 100);
-
-  } else if (toolChoice == '3') {
-    image(img3, mouseX - 100, mouseY - 50, 200, 100);
-
-  } else if (toolChoice == '4') {
-    image(img4, mouseX - 100, mouseY - 50, 200, 100);
-
-  } else if (toolChoice == '5') {
-    image(img5, mouseX - 100, mouseY - 50, 200, 100);
-
-  } else if (toolChoice == '6') {
-    image(img6, mouseX - 100, mouseY - 50, 200, 100);
-
-  } else if (toolChoice == '7') {
-    image(img7, mouseX - 100, mouseY - 50, 200, 100);
-
-  } else if (toolChoice == '8') {
-    image(img8, mouseX - 100, mouseY - 50, 200, 100);
-
-  } else if (toolChoice == '9') {
-    image(img9, mouseX - 100, mouseY - 50, 200, 100);
-
-  } else if (toolChoice == '0') {
-    image(img0, mouseX - 100, mouseY - 50, 200, 100);
-
-  } else if (toolChoice == 'g' || toolChoice == 'G') {
-    image(img, mouseX - 50, mouseY - 50, 50, 50);
-
-  } else if (toolChoice == 'j' || toolChoice == 'J') {
-    image(img2, mouseX - 60, mouseY - 60, 60, 60);
-  }
-}
-
-function clear_print() {
-  if (key == 'x' || key == 'X') {
-    image(screenbg, 0, 0, width, height);
+function keyPressed() {
+  if (key == '1') {
+    choice = '1';
+    currentColor = color(255, 0, 0);
+  } else if (key == '2') {
+    choice = '2';
+    currentColor = color(128, 0, 255);
+  } else if (key == '3') {
+    choice = '3';
+    currentColor = color(255, 255, 0);
+  } else if (key == '4') {
+    choice = '4';
+    currentColor = color(0, 0, 0);
+  } else if (key == '5') {
+    choice = '5';
+    currentColor = color(0, 255, 255);
+  } else if (key == '6') {
+    choice = '6';
+    currentColor = color(0, 0, 255);
+  } else if (key == '7') {
+    choice = '7';
+    currentColor = color(0, 255, 0);
+  } else if (key == '8') {
+    choice = '8';
+    currentColor = color(255, 165, 0);
+  } else if (key == '9') {
+    choice = '9';
+    currentColor = color(255, 105, 180);
+  } else if (key == '0') {
+    choice = '0';
+    currentColor = color(255);
+  } else if (key == 'x' || key == 'X') {
+    background(245);
   } else if (key == 'p' || key == 'P') {
-    saveme();
+    saveCanvas(initials + day() + hour() + minute() + second(), 'jpg');
   }
-}
-
-function saveme() {
-  let filename = initials + day() + hour() + minute() + second();
-  if (second() != lastscreenshot) {
-    saveCanvas(filename, 'jpg');
-    key = "";
-  }
-  lastscreenshot = second();
 }
